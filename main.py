82d0b5dafa7ca25e20e09610acf801bfbf7a1c7f
steve23tkn <76933993+steve23tkn@users.noreply.github.com>
2021-07-20 20:10:26 +0700

    add sync address routines

---
+++ main.py
@@ -0,0 +1,53 @@
+import mysql.connector
+import config as cf
+from tqdm import tqdm
+
+coredb = mysql.connector.connect(
+    host=cf.core_host,
+    user=cf.core_user,
+    password=cf.core_pass,
+    database=cf.core_db
+)
+
+localdb = mysql.connector.connect(
+    host=cf.local_host,
+    user=cf.local_user,
+    password=cf.local_pass,
+    database=cf.local_db
+)
+
+def RetrieveAddressesExchange():
+    mycursor = coredb.cursor()
+    sql = f"select * from user_coin_addresses where coin='eth'"
+    mycursor.execute(sql)
+    rows = mycursor.fetchall()
+    return rows # type: list, len: 18072
+
+def RetrieveAddressesLocal():
+    mycursor = localdb.cursor()
+    sql = f"select * from addresses"
+    mycursor.execute(sql)
+    rows = mycursor.fetchall()
+    return rows # type: list, 
+
+def InjectAddresses(addresses):
+    mycursor = localdb.cursor()
+    for addr in tqdm(addresses):
+        sql = f"insert into addresses (address) values ('{addr}')"
+        mycursor.execute(sql)
+    localdb.commit() 
+
+
+user_coin_addresses = RetrieveAddressesExchange()
+local_addresses = RetrieveAddressesLocal()
+
+n_exch = len(user_coin_addresses)
+n_local = len(local_addresses)
+
+print(f'processing {n_exch} addresses')
+
+new_addr = [addr for addr in user_coin_addresses if addr in local_addresses]
+
+InjectAddresses(new_addr)
+
+print(f'new addresses processed')
